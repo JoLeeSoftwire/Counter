1743f189a73bd40f42f67cf91bfb16b0c3197429
JoLeeSoftwire <Jo.Lee@softwire.com>
2020-01-16 12:22:08 +0000

    boxes can get count of countable contents

--- src/com/company/Box.java
+++ src/com/company/Box.java
@@ -2,6 +2,8 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Box<T> implements Countable {
     private List<T> items;
@@ -12,7 +14,15 @@ public class Box<T> implements Countable {
 
     @Override
     public int getCount() {
-        return items.size();
+//        List<Countable> countable = items.stream().filter(s -> s instanceof Countable).map(i -> (Countable) i).collect(Collectors.toList());
+//        int totalCountable = countable.stream().mapToInt(i -> i.getCount()).sum();
+//        int totalNonCountable = items.size() - countable.size();
+//        return totalCountable + totalNonCountable;
+
+        Map <Boolean, List<T>> countableOrNot = items.stream().collect(Collectors.partitioningBy(s -> s instanceof Countable));
+        int totalCountable = countableOrNot.get(true).stream().mapToInt(i -> ((Countable) i).getCount()).sum();
+        int totalNonCountable = countableOrNot.get(false).size();
+        return totalCountable + totalNonCountable;
     }
 
     public void add(T item) {
